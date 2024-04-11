d4b2490847e7d1e8c68dfaca7f7c11eb30631745
vidu-125shi <154583551+vidu-125shi@users.noreply.github.com>
2024-04-11 14:42:29 +0530

    Add files via upload

---
+++ week2Q2.c
@@ -0,0 +1,33 @@
+
+#include<stdio.h>
+void main()
+{
+    int n,t;
+    scanf("%d",&t);
+    int f=1;
+    while(t!=0)
+    {
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i;j<n;j++){
+            int k=j+1;
+            if(a[i]+a[j] == a[k]){
+                printf("%d %d %d\n",i+1,j+1,k+1);f=0;
+            }
+        }
+    }
+   
+    if(f==1){
+    printf("No sequence found");
+    
+    }
+    t--;
+    }
+}
