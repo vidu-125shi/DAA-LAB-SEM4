8224c6d8843c32394a7e4e767e7c460d76d75168
vidu-125shi <154583551+vidu-125shi@users.noreply.github.com>
2024-04-19 22:08:22 +0530

    Create containerWithmostWater.cpp

---
+++ containerWithmostWater.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxArea(vector<int>& height) 
+    {
+        int i = 0, j = height.size()-1;
+        int maxWater = 0;
+        while(j>i)    
+        {
+            maxWater = max(min(height[i], height[j]) * (j-i), maxWater);
+            (height[i] <height[j])?i++:j--;
+        }
+        return maxWater;
+    }
+};
+
+//this is from leetcode
